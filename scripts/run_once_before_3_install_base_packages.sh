f7cc811ef6e2a6c09affbc37ffd1b34d89acf888
Cody Nguyen <cody@wealthica.com>
2022-11-02 22:42:39 +0700

    save

--- scripts/run_once_before_3_install_base_packages.sh
+++ scripts/run_once_before_3_install_base_packages.sh
@@ -8,8 +8,8 @@ then
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+        bullseye stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt update
     sudo apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 elif command -v dnf &> /dev/null;
