fdf6590ae662c19be1cbdf0f577cf46c1ef74fa2
Cody Nguyen <cody@wealthica.com>
2022-11-02 22:35:37 +0700

    save

--- scripts/run_once_before_2_install_password_managers.sh
+++ scripts/run_once_before_2_install_password_managers.sh
@@ -3,11 +3,11 @@
 if command -v apt &> /dev/null;
 then
     echo "apt install lastpass-cli"
-    sudo apt install lastpass-cli
+    sudo apt -y install lastpass-cli
 elif command -v dnf &> /dev/null;
 then
     echo "dnf install lastpass-cli"
-    sudo dnf install lastpass-cli
+    sudo dnf -y install lastpass-cli
 elif command -v brew &> /dev/null;
 then
     echo "brew install lastpass-cli"
