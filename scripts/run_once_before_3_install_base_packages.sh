a62ec67b7131d64b14a25105cb75ec1214d2a716
Cody Nguyen <cody@wealthica.com>
2022-11-04 00:29:18 +0700

    save

--- scripts/run_once_before_3_install_base_packages.sh
+++ scripts/run_once_before_3_install_base_packages.sh
@@ -1,29 +1,15 @@
 #!/bin/sh
 
+echo "docker"
+curl -fsSL https://get.docker.com -o get-docker.sh | sudo sh
+
 if command -v apt &> /dev/null;
 then
-    # Docker
-    echo "apt docker"
-    sudo apt -y install ca-certificates curl gnupg lsb-release
-    sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-        bullseye stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt update
-    sudo apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    echo "apt"
 elif command -v dnf &> /dev/null;
 then
-    # Docker
-    echo "dnf docker"
-    sudo dnf -y install dnf-plugins-core
-    sudo dnf config-manager \
-        --add-repo \
-        https://download.docker.com/linux/fedora/docker-ce.repo
-    sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-    sudo systemctl start docker
+    echo "dnf"
 elif command -v brew &> /dev/null;
 then
-    echo "brew docker"
-    brew cask install docker
+    echo "brew"
 fi
