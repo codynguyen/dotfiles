b30a456d637bc3f5d71afb02ae2005cfcbddeeb1
Cody Nguyen <cody@wealthica.com>
2022-12-03 19:58:14 +0700

    Update run_once_before_1_setup_os.sh

--- scripts/run_once_before_1_setup_os.sh
+++ scripts/run_once_before_1_setup_os.sh
@@ -5,22 +5,22 @@ then
     echo "apt found. updating system"
     sudo apt update && sudo apt -y upgrade
     sudo apt -y install zsh
+    echo "changing shell to zsh"
+    chsh -s $(which zsh)
 elif command -v dnf &> /dev/null
 then
     echo "dnf found. updating system"
     sudo dnf -y up --refresh
     sudo dnf -y install zsh
+    echo "changing shell to zsh"
+    chsh -s $(which zsh)
 elif command -v xcode-select &> /dev/null
 then
-    echo "mac. updating system"
-    xcode-select --install
+    # echo "mac. updating system"
+    # xcode-select --install
 
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-    brew install --cask iterm2
-    brew install zsh
-    
+    # brew install --cask iterm2
+    # brew install zsh
 fi
-
-echo "changing shell to zsh"
-chsh -s $(which zsh)
