23cee6f7a63e22e2170a85a82b8cd1cf9bfdc147
Cody Nguyen <cody@wealthica.com>
2022-12-03 20:08:08 +0700

    Update run_once_before_3_install_base_packages.sh

--- scripts/run_once_before_3_install_base_packages.sh
+++ scripts/run_once_before_3_install_base_packages.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-echo "docker"
-curl -fsSL https://get.docker.com | sudo sh
-
-if command -v apt &> /dev/null;
+if command -v apt-get &> /dev/null;
 then
     echo "apt"
+    echo "docker"
+    curl -fsSL https://get.docker.com | sudo sh
 elif command -v dnf &> /dev/null;
 then
     echo "dnf"
+    echo "docker"
+    curl -fsSL https://get.docker.com | sudo sh
 elif command -v brew &> /dev/null;
 then
     echo "brew"
+
 fi
